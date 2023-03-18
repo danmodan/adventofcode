b460bc86d4537950df50a6223beec96247264220
danmodan <danmodan92@gmail.com>
2023-03-18 20:23:19 -0300

    short grid

--- 2015/day6/LitBulb.java
+++ 2015/day6/LitBulb.java
@@ -1,6 +1,7 @@
 package day6;
 
 import java.io.BufferedReader;
+import java.io.IOException;
 import java.io.InputStream;
 import java.io.InputStreamReader;
 import java.net.URL;
@@ -8,18 +9,44 @@ import java.net.URLConnection;
 import java.util.ArrayList;
 import java.util.List;
 import java.util.NoSuchElementException;
-import java.util.function.Function;
 import java.util.regex.Matcher;
 import java.util.regex.Pattern;
 
 public class LitBulb {
 
-    
-    private static final BulbGrid BULB_GRID = new BulbGrid();
+    private static final String REGEX = "^(toggle|turn off|turn on)|(\\d{0,3},\\d{0,3}(?= through))|((?<=through )\\d{0,3},\\d{0,3})";
+    private static final String SHORT_INPUT_FILE_PATH = "file:day6/short-input.txt";
+    private static final String MEDIUM_INPUT_FILE_PATH = "file:day6/medium-input.txt";
+    private static final String INPUT_FILE_PATH = "file:day6/input.txt";
 
     public static void main(String[] args) throws Exception {
 
-        URL url = new URL("file:day6/input2.txt");
+        runWithShortInput();
+    }
+
+    private static void runWithShortInput() throws IOException {
+
+        List<Command> commands = getCommandList(SHORT_INPUT_FILE_PATH);
+
+        ShortBulbGrid shortBulbGrid = new ShortBulbGrid();
+
+        commands.forEach(shortBulbGrid::process);
+
+        for(int i = shortBulbGrid.grid.length - 1 ; i >= 0; i--) {
+            int row = shortBulbGrid.grid[i];
+            System.out.println(
+                String
+                    .format("%1$32s", Integer.toBinaryString(row))
+                    .replace(' ', '0')
+                    .substring(0, shortBulbGrid.grid.length)
+                    .replace('1', '#')
+                    .replace('0', '.')
+            );
+        }
+    }
+
+    private static List<Command> getCommandList(String inputFilePath) throws IOException {
+        URL url = new URL(inputFilePath);
         URLConnection conn = url.openConnection();
 
         try (
@@ -27,7 +54,7 @@ public class LitBulb {
                 InputStreamReader isr = new InputStreamReader(is);
                 BufferedReader br = new BufferedReader(isr);) {
 
-            Pattern pattern = Pattern.compile("^(toggle|turn off|turn on)|(\\d{0,3},\\d{0,3}(?= through))|((?<=through )\\d{0,3},\\d{0,3})");
+            Pattern pattern = Pattern.compile(REGEX);
 
             List<Command> commands = new ArrayList<>();
 
@@ -48,46 +75,20 @@ public class LitBulb {
                 commands.add(new Command(action, point1, point2));
             }
 
-            BulbGrid bulbGrid = new BulbGrid();
-
-            commands.forEach(bulbGrid::process);
-
-            for(int i = bulbGrid.grid.length - 1 ; i >= 0; i--) {
-                System.out.println(
-                    String.format("%1$" + bulbGrid.grid.length + "s", Integer.toBinaryString(bulbGrid.grid[i])).replace(' ', '0')
-                );
-            }
+            return commands;
         }
     }
 }
 
-class BulbGrid {
+class ShortBulbGrid {
 
     final int[] grid = new int[10];
 
-    BulbGrid() {
-        for(int i = 0; i < grid.length; i++) {
-            grid[i] = 0;
-        }
-    }
+    private int createMask(int fromX, int toX) {
 
-    private int createMask(int fromX, int toX, Function<Boolean, Character> getBitFunction) {
-
-        StringBuilder sb = new StringBuilder();
-        for(int j = 0; j < grid.length; j++) {
-            sb.append(getBitFunction.apply(fromX <= j && j <= toX));
-        }
-        return Integer.parseInt(sb.toString(), 2);
-    }
-
-    private int createAndMask(int fromX, int toX) {
-
-        return createMask(fromX, toX, bool -> bool ? '0' : '1');
-    }
-
-    private int createOrMask(int fromX, int toX) {
-
-        return createMask(fromX, toX, bool -> bool ? '1' : '0');
+        return fromX == 0 ? 
+            -1 << (31 - toX) : 
+            (-1 << -fromX) ^ (-1 << (31 - toX));
     }
 
     synchronized void process(Command command) {
@@ -97,26 +98,23 @@ class BulbGrid {
 
         int fromX = Math.min(point1.x, point2.x);
         int toX = Math.max(point1.x, point2.x);
-        int andMask = createAndMask(fromX, toX);
-        int orMask = createOrMask(fromX, toX);
+        int mask = createMask(fromX, toX);
         
         int fromY = Math.min(point1.y, point2.y);
         int toY = Math.max(point1.y, point2.y);
         for(int i = fromY; i <= toY; i++) {
 
-            int strip = grid[i];
-
             switch (command.action) {
                 case TOGGLE: {
-                    grid[i] = strip ^ orMask;
+                    grid[i] ^= mask;
                     break;
                 }
                 case TURN_OFF: {
-                    grid[i] = strip & andMask;
+                    grid[i] &= (~mask);
                     break;
                 }
                 case TURN_ON: {
-                    grid[i] = strip | orMask;
+                    grid[i] |= mask;
                     break;
                 }
             }
